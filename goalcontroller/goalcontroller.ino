c2fbc9e58905f6e93aa6d71b37b08946ece0b169
Justin Jantzi <jwjantzi@rochester.rr.com>
2021-12-29 22:47:48 -0500

    Non dumb code for brother school project

--- goalcontroller/goalcontroller.ino
+++ goalcontroller/goalcontroller.ino
@@ -9,27 +9,41 @@ void setup()
   Serial.begin(9600);
 }
 
-int cooldown = COOLDOWN_TIME;
+int cooldown = 0;
 float prevVal1 = 0;
 float prevVal2 = 0;
+
+float lastTime = 0;
+
+void countdown() {
+ lastTime = millis()
+  
+ if (cooldown > 0){
+    cooldown--;
+  } 
+}
 
 void loop()
 {
   float val1 = analogRead(LDR_1_PIN);
   float valDiff1 = prevVal1 - val1;
-  if (valDiff1 > 30 && cooldown == COOLDOWN_TIME && prevVal1 > 650) {
+  if (valDiff1 > 30 && cooldown == 0 && prevVal1 > 650) {
     goal(1);
   }
+  
   float val2 = analogRead(LDR_2_PIN);
   float valDiff2 = prevVal2 - val2;
-  if (valDiff2 > 30 && cooldown == COOLDOWN_TIME && prevVal2 > 650) {
+  if (valDiff2 > 30 && cooldown == 0 && prevVal2 > 650) {
     goal(2);
   }
-  if (cooldown > 0 && cooldown != COOLDOWN_TIME){
-    cooldown--;
-  } else if (cooldown == 0) {
-    cooldown = COOLDOWN_TIME;
-  }
+  
+  float time = millis()
+  
+   if(time - lastTime > 1000) {
+     countdown();
+   }
+   
+    
   prevVal1 = val1;
   prevVal2 = val2;
   delay(100);
@@ -40,5 +54,5 @@ void goal(int goalId){
   tone(BUZ_PIN, 1000);
   delay(150);
   noTone(BUZ_PIN);
-  cooldown--;
+  cooldown = COOLDOWN_TIME;
 }
